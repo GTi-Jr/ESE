40414e96fad079599f05ae0084f27a51a9fd87eb
Gui Carneiro <gui.carneiro15@gmail.com>
2014-08-08 21:54:12 -0300

    impoved code at application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,7 @@ class ApplicationController < ActionController::Base
   helper_method :loged_in?
 
   def check_and_redirect
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    if @current_user == nil
+    unless loged_in?
       redirect_to "/log_in", :notice => "Increva-se e faça log in para ter acesso"
     end
   end
