8087e9995b0d619f7faee904c4450684db258edb
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-05-23 00:41:42 -0300

    Change site_url var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'www.gti-sistemadacivil.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.project_url }
 
 end
