74acbdaff9cc777ab77d6a77de96b38941cf0428
caioeps <caioergos@gmail.com>
2016-05-22 22:03:27 -0300

    port 587

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '25',
+  :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENGRID_USERNAME'],
   :password       => ENV['SENGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
-
-ActionMailer::Base.delivery_method = :smtp
